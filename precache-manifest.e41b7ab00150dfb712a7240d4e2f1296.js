3df189b7dfd7d994974e3253127c8a6a8aed5c12
root <root@LAPTOP-6NNPB1J2.localdomain>
2020-07-11 17:06:37 +0530

    Updates

---
+++ precache-manifest.e41b7ab00150dfb712a7240d4e2f1296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "912f9db35741b487c96cdae214472f80",
+    "url": "/Dictionary/index.html"
+  },
+  {
+    "revision": "d0eab921a7da1bb2ebf5",
+    "url": "/Dictionary/static/css/main.7631857d.chunk.css"
+  },
+  {
+    "revision": "d8d5a246415e06d5dea3",
+    "url": "/Dictionary/static/js/2.abba224a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Dictionary/static/js/2.abba224a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0eab921a7da1bb2ebf5",
+    "url": "/Dictionary/static/js/main.f704861c.chunk.js"
+  },
+  {
+    "revision": "aaae350361e8996d7a9c",
+    "url": "/Dictionary/static/js/runtime-main.b151d5db.js"
+  }
+]);
