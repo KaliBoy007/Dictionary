f90f705c9d32f6ca87d794153aec2201be513aa3
kaliboy011101@gmail.com <lokesh19055@cse.ss.edu.in>
2020-07-14 20:22:45 +0530

    almost done with the app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <h1 className="header-container">THE MASTER DICTIONARY</h1>;
+  return <h1 className="header-container">THE ENCYCLOPEDIA OF WORDS</h1>;
 };
 
 export default Header;
